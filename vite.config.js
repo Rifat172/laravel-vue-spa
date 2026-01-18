8581a6753ce5f25ba14763b6cdf3214b63e3f28b
Rifat <172rifat722@gmail.com>
2026-01-18 14:15:20 +0500

    npm install @vitejs/plugin-vue --save-dev

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [
@@ -9,6 +10,13 @@ export default defineConfig({
             refresh: true,
         }),
         tailwindcss(),
+    ],
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        vue(),
     ],
     server: {
         watch: {
